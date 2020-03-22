fd2d1c4c8f8be7d328842713028c1ad43a8c178d
ManiKatwal <62191180+ManiKatwal@users.noreply.github.com>
2020-03-22 17:48:41 -0500

    Day three changes and added NewClass_3 and remove NewClass

--- Calculator/src/MyThread/NewClass3.java
+++ Calculator/src/MyThread/NewClass3.java
@@ -1,10 +1,10 @@
 package MyThread;
 
-public class NewClass {
+public class NewClass3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+
 	}
 
 }
